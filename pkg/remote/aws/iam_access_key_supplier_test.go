017567f849920c6ede23cd243fb326c9533cd812
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-04-08 17:32:33 +0200

    Issue 165: Delete hidden dependency for iam_access_key_supplier

--- pkg/remote/aws/iam_access_key_supplier_test.go
+++ pkg/remote/aws/iam_access_key_supplier_test.go
@@ -4,14 +4,12 @@ import (
 	"context"
 	"testing"
 
-	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
-
-	resourceaws "github.com/cloudskiff/driftctl/pkg/resource/aws"
-
 	"github.com/aws/aws-sdk-go/aws/awserr"
 
 	"github.com/cloudskiff/driftctl/pkg/parallel"
-
+	"github.com/cloudskiff/driftctl/pkg/remote/aws/repository"
+	remoteerror "github.com/cloudskiff/driftctl/pkg/remote/error"
+	resourceaws "github.com/cloudskiff/driftctl/pkg/resource/aws"
 	awsdeserializer "github.com/cloudskiff/driftctl/pkg/resource/aws/deserializer"
 
 	"github.com/cloudskiff/driftctl/test/goldenfile"
@@ -20,11 +18,10 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/iam"
 
-	mocks2 "github.com/cloudskiff/driftctl/test/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	"github.com/cloudskiff/driftctl/mocks"
+	mocks2 "github.com/cloudskiff/driftctl/test/mocks"
 
 	"github.com/cloudskiff/driftctl/pkg/resource"
 	"github.com/cloudskiff/driftctl/pkg/terraform"
@@ -36,113 +33,47 @@ func TestIamAccessKeySupplier_Resources(t *testing.T) {
 	cases := []struct {
 		test    string
 		dirName string
-		mocks   func(client *mocks.FakeIAM)
+		mocks   func(repo *repository.MockIAMRepository)
 		err     error
 	}{
 		{
 			test:    "no iam access_key",
 			dirName: "iam_access_key_empty",
-			mocks: func(client *mocks.FakeIAM) {
-				client.On("ListUsersPages",
-					&iam.ListUsersInput{},
-					mock.MatchedBy(func(callback func(res *iam.ListUsersOutput, lastPage bool) bool) bool {
-						callback(&iam.ListUsersOutput{Users: []*iam.User{
-							{
-								UserName: aws.String("test-driftctl"),
-							},
-						}}, true)
-						return true
-					})).Return(nil)
-				client.On("ListAccessKeysPages", mock.Anything, mock.Anything).Return(nil)
+			mocks: func(repo *repository.MockIAMRepository) {
+				repo.On("ListAllAccessKeys").Return([]*iam.AccessKeyMetadata{}, nil)
 			},
 			err: nil,
 		},
 		{
 			test:    "iam multiples keys for multiples users",
 			dirName: "iam_access_key_multiple",
-			mocks: func(client *mocks.FakeIAM) {
-				client.On("ListUsersPages",
-					&iam.ListUsersInput{},
-					mock.MatchedBy(func(callback func(res *iam.ListUsersOutput, lastPage bool) bool) bool {
-						callback(&iam.ListUsersOutput{Users: []*iam.User{
-							{
-								UserName: aws.String("test-driftctl"),
-							},
-							{
-								UserName: aws.String("test-driftctl2"),
-							},
-						}}, true)
-						return true
-					})).Return(nil)
-				client.On("ListAccessKeysPages",
-					&iam.ListAccessKeysInput{
-						UserName: aws.String("test-driftctl"),
+			mocks: func(repo *repository.MockIAMRepository) {
+				repo.On("ListAllAccessKeys").Return([]*iam.AccessKeyMetadata{
+					{
+						AccessKeyId: aws.String("AKIA5QYBVVD26EJME25D"),
+						UserName:    aws.String("test-driftctl2"),
 					},
-					mock.MatchedBy(func(callback func(res *iam.ListAccessKeysOutput, lastPage bool) bool) bool {
-						callback(&iam.ListAccessKeysOutput{AccessKeyMetadata: []*iam.AccessKeyMetadata{
-							{
-								AccessKeyId: aws.String("AKIA5QYBVVD223VWU32A"),
-								UserName:    aws.String("test-driftctl"),
-							},
-						}}, false)
-						callback(&iam.ListAccessKeysOutput{AccessKeyMetadata: []*iam.AccessKeyMetadata{
-							{
-								AccessKeyId: aws.String("AKIA5QYBVVD2QYI36UZP"),
-								UserName:    aws.String("test-driftctl"),
-							},
-						}}, true)
-						return true
-					})).Return(nil)
-				client.On("ListAccessKeysPages",
-					&iam.ListAccessKeysInput{
-						UserName: aws.String("test-driftctl2"),
+					{
+						AccessKeyId: aws.String("AKIA5QYBVVD223VWU32A"),
+						UserName:    aws.String("test-driftctl"),
 					},
-					mock.MatchedBy(func(callback func(res *iam.ListAccessKeysOutput, lastPage bool) bool) bool {
-						callback(&iam.ListAccessKeysOutput{AccessKeyMetadata: []*iam.AccessKeyMetadata{
-							{
-								AccessKeyId: aws.String("AKIA5QYBVVD26EJME25D"),
-								UserName:    aws.String("test-driftctl2"),
-							},
-						}}, false)
-						callback(&iam.ListAccessKeysOutput{AccessKeyMetadata: []*iam.AccessKeyMetadata{
-							{
-								AccessKeyId: aws.String("AKIA5QYBVVD2SWDFVVMG"),
-								UserName:    aws.String("test-driftctl2"),
-							},
-						}}, true)
-						return true
-					})).Return(nil)
+					{
+						AccessKeyId: aws.String("AKIA5QYBVVD2QYI36UZP"),
+						UserName:    aws.String("test-driftctl"),
+					},
+					{
+						AccessKeyId: aws.String("AKIA5QYBVVD2SWDFVVMG"),
+						UserName:    aws.String("test-driftctl2"),
+					},
+				}, nil)
 			},
 			err: nil,
 		},
 		{
-			test:    "Cannot list iam user",
-			dirName: "iam_access_key_empty",
-			mocks: func(client *mocks.FakeIAM) {
-				client.On("ListUsersPages",
-					&iam.ListUsersInput{},
-					mock.MatchedBy(func(callback func(res *iam.ListUsersOutput, lastPage bool) bool) bool {
-						return true
-					})).Return(awserr.NewRequestFailure(nil, 403, ""))
-				client.On("ListAccessKeysPages", mock.Anything, mock.Anything).Return(awserr.NewRequestFailure(nil, 403, ""))
-			},
-			err: remoteerror.NewResourceEnumerationErrorWithType(awserr.NewRequestFailure(nil, 403, ""), resourceaws.AwsIamAccessKeyResourceType, resourceaws.AwsIamUserResourceType),
-		},
-		{
 			test:    "Cannot list iam access_key",
 			dirName: "iam_access_key_empty",
-			mocks: func(client *mocks.FakeIAM) {
-				client.On("ListUsersPages",
-					&iam.ListUsersInput{},
-					mock.MatchedBy(func(callback func(res *iam.ListUsersOutput, lastPage bool) bool) bool {
-						callback(&iam.ListUsersOutput{Users: []*iam.User{
-							{
-								UserName: aws.String("test-driftctl"),
-							},
-						}}, true)
-						return true
-					})).Return(nil)
-				client.On("ListAccessKeysPages", mock.Anything, mock.Anything).Return(awserr.NewRequestFailure(nil, 403, ""))
+			mocks: func(repo *repository.MockIAMRepository) {
+				repo.On("ListAllAccessKeys").Return(nil, awserr.NewRequestFailure(nil, 403, ""))
 			},
 			err: remoteerror.NewResourceEnumerationError(awserr.NewRequestFailure(nil, 403, ""), resourceaws.AwsIamAccessKeyResourceType),
 		},
@@ -162,7 +93,7 @@ func TestIamAccessKeySupplier_Resources(t *testing.T) {
 		}
 
 		t.Run(c.test, func(tt *testing.T) {
-			fakeIam := mocks.FakeIAM{}
+			fakeIam := repository.MockIAMRepository{}
 			c.mocks(&fakeIam)
 
 			provider := mocks2.NewMockedGoldenTFProvider(c.dirName, providerLibrary.Provider(terraform.AWS), shouldUpdate)
